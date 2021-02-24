da89a0edd21ccd1b08340a8f8e27b365f18f69bd
Darkmift <Darkmift@users.noreply.github.com>
2021-02-24 11:45:45 +0200

    work on email app structure

---
+++ js/apps/mail/cmps/email-left-menu.cmp.js
@@ -0,0 +1,13 @@
+export default {
+    template: /*html*/`
+        <div>
+            <button>Compose</button>
+            <button>Inbox</button>
+            <button>Marked</button>
+            <button>Trash</button>
+        </div>`,
+    data() {
+        return {};
+    },
+    methods: {},
+};
---
+++ js/apps/mail/cmps/email-list.cmp.js
@@ -0,0 +1,7 @@
+export default {
+    template: `<div>EMAIL LIST</div>`,
+    data() {
+        return {};
+    },
+    methods: {},
+};
