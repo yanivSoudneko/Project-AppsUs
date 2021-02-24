e6a09bc602c7752a158d05288d9249055dc54aeb
Yaniv <soudneko@icloud.com>
2021-02-24 10:23:16 +0200

    nabar addec with links

--- js/cmps/header.cmp.js
+++ js/cmps/header.cmp.js
@@ -1,5 +1,27 @@
 export default {
-    template: `<div>HEADER</div>`,
+    template: ` 
+    <nav class="navbar bg-primary">
+    <div class="main-size flex j-between a-center nav-wrapper">
+        <h3 class="logo" @click="$router.push('/')">AppSus</h3>
+        <div class="nav-bar-tabs clean-list flex">
+            <router-link active-class="active-link" class="clean-anchor nav-link flex a-center j-center" to="/" exact>
+                Home
+            </router-link>
+            <router-link to="/book" class="clean-anchor nav-link flex a-center j-center" exact>
+                Books
+            </router-link>
+            <router-link to="/keep" class="clean-anchor nav-link flex a-center j-center" exact>
+                Keep
+            </router-link>
+            <router-link to="/mail" class="clean-anchor nav-link flex a-center j-center" exact>
+                Mail
+            </router-link>
+            <router-link to="/about" class="clean-anchor nav-link flex a-center j-center" exact>
+                About
+            </router-link>
+        </div>
+    </div>
+</nav>`,
     data() {
         return {};
     },
