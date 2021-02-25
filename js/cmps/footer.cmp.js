58509585b6bbc4f89dd26f9708ef7c4af985029c
Yaniv <soudneko@icloud.com>
2021-02-25 16:37:52 +0200

    media queries

--- js/cmps/footer.cmp.js
+++ js/cmps/footer.cmp.js
@@ -1,7 +1,8 @@
 export default {
     template: `<footer class="flex a-center j-center column">
         <h2>AppSus.</h2>
-        <div>&copy;2021 Project made by Avi Koeing & Yaniv Soudneko</div>
+        <div>&copy;2021 </div>
+           <p> Project made by Avi Koeing & Yaniv Soudneko</p>
         <div class="social-icons">
             <i href="#" class="fab fa-facebook-square"></i>
             <i href="#" class="fab fa-github-square"></i>
